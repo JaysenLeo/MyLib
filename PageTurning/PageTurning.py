c5bc254a1bed11c43323e16b4bedd26f1d042de7
LeeChengjie <lichengjie@cashep.cn>
2017-12-27 16:59:05 +0800

    """

--- PageTurning/PageTurning.py
+++ PageTurning/PageTurning.py
@@ -1,4 +1,4 @@
-# _*_coding:utf-8_*_
+# -*- coding: utf-8 -*-
 """
 ModelIntroduction:
 
@@ -17,27 +17,29 @@ from LogModule.conf import logconf
 logging.config.fileConfig(logconf.L_URL.get('log_url_upTool'))
 logger = logging.getLogger("root")
 def page_generator(page_content=None, page_size=9, page_current=1, page_opt='', page_jump=None):
-    """
-    :param
-            page_content : content of page that is list
+    '''
+    参数:
+            page_content : 以列表形式组织成的页内容
             page_size : length of each page (how many items of list)
             page_current : position of current page
             page_option :
-                            page_first
-                            page_last
-                            page_up
-                            page_down
-                            page_jump
-    """
+                            page_first 翻到首页
+                            page_last 翻到最后一页
+                            page_up 翻到上一页
+                            page_down 翻到下一页
+                            page_jump 跳到某一页
+    '''
     #logger.info("\x1B[1;32;40m********************PageTurning********************\x1B[0m")
     logger.info("********************PageTurning********************")
     try:
-        # Initialize the page content
+        # 初始化页内容
         if page_content is None:
             page_content = []
-
+        if not isinstance(page_content, list):
+            pass
+            # raise
         page_count = len(page_content)
-        # Statistics of the total number of pages
+        # 统计总页数
         if page_count % page_size > 0:
             page_total = (page_count / page_size) + 1
         else:
@@ -48,11 +50,11 @@ def page_generator(page_content=None, page_size=9, page_current=1, page_opt='', 
         else:
             page_current_int = 1
 
-        # The total number of pages 1
+        # 总页数为1
         if page_total is 1:
             page_current_int = 1
-        # More than 1 of the total number of pages
-        else:
+        # 总页数超过1
+        elif page_jump is None and page_total > 1:
             if page_opt == 'page_first':
                 page_current_int = 1
             elif page_opt == 'page_last':
@@ -67,6 +69,10 @@ def page_generator(page_content=None, page_size=9, page_current=1, page_opt='', 
                     page_current_int = page_total
                 else:
                     page_current_int += 1
+        elif page_jump is not None:
+            page_current_int = page_jump
+        else:
+            pass
 
         # The content of the page to obtain the corresponding pages.
         if len(page_content) < page_size:
@@ -81,7 +87,7 @@ def page_generator(page_content=None, page_size=9, page_current=1, page_opt='', 
                 'page_msg': {'page_count': page_count, 'page_total': page_total, 'page_current': str(page_current_int)}}
     except Exception as e:
         logging.exception("[Errors]|<PageTurning>Turing Page Errors:" + str(e))
-        pass
+
 
 def page_content_sort():
     pass
@@ -89,16 +95,16 @@ def page_content_sort():
 
 if __name__ == '__main__':
     test_list = range(33)
-    while True:
-        # print "\x1B[1;32;40m" +str(test_list) +"\x1B[0m"
-        print '\033[1;31;40m' +str(test_list) +'\033[0m'
-        # page_size = int(raw_input('page_size:'))
-        # page_current = int(raw_input('page_current:'))
-        page_opt = raw_input('page_option:')
-        page_jump=None
-        """ page_first;page_last;page_up;page_down;page_jump
-        """
-        print page_generator(test_list,page_size=3,page_current=1,page_opt=page_opt,page_jump=None)
+    # while True:
+    # print "\x1B[1;32;40m" +str(test_list) +"\x1B[0m"
+    print '\033[1;31;40m' +str(test_list) +'\033[0m'
+    # page_size = int(raw_input('page_size:'))
+    # page_current = int(raw_input('page_current:'))
+    page_opt=None
+    page_jump=2
+    """ page_first;page_last;page_up;page_down;page_jump
+    """
+    print page_generator(test_list,page_size=3,page_current=1,page_opt=page_opt,page_jump=page_jump)
 
 
 
